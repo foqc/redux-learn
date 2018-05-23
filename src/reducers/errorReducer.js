a7f278aac482523bdced54c38fb16cd23593ca38
Fabian Quijosaca <fabianq44@gmail.com>
2018-05-23 17:53:15 -0500

    error reducer that srores all API request errors

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -1,14 +1,12 @@
-import * as types from '../actions/actionTypes';
-import initialState from '../reducers/initialState';
+export const errorReducer = (state = {}, action) => {
+  const { type, payload } = action;
+  const matches = /(.*)_(REQUEST|FAILURE)/.exec(type);
 
-export default function getBookFailure(state = initialState.error, action) {
-    switch (action.type) {
-      case 'GET_BOOKS_FAILURE':
-        return action.error;
-      case 'LOAD_BOOKS_SUCCESS1':
-        return action.error;
-  
-      default:
-        return state;
-    }
-  }
+  if (!matches) return state;
+
+  const [, requestName, requestState] = matches;
+
+  return {
+    ...state, [requestName]: requestState === 'FAILURE' ? payload.message : ''
+  };
+};
